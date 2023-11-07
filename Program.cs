aab2caf4b14b7b5fe367dc30e100c299d32343ab
Tiago Nunes <tiagoo2001@yahoo.com.br>
2023-11-07 00:32:45 -0300

    Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
-﻿using DesafioPOO.Models;
+﻿using DesafioPOO.Models; 
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia Modelo", "123456", 64);
+            Iphone iphone = new Iphone("987654321", "iPhone Modelo", "987654", 128);
+
+            Console.WriteLine("Teste Smartphone Nokia:");
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+            nokia.ReceberLigacao();
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            Console.WriteLine("Teste Smartphone Iphone:");
+            iphone.Ligar();
+            iphone.InstalarAplicativo("Instagram");
+            iphone.ReceberLigacao();
+
+            Console.ReadLine();
+        }
+    }
+}
